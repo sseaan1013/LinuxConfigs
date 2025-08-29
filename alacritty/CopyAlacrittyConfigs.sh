1c00d13c6346b1ddd0d1743784fdf9c2154c3517
Sean Phelan <sean.phelan1013@gmail.com>
2025-08-29 09:20:05 -0400

    Update and rename AlacrittyConfigs.sh to CopyAlacrittyConfigs.sh

---
+++ alacritty/CopyAlacrittyConfigs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p $HOME/.config/alacritty
+cp $HOME/LinuxConfigs/alacritty/alacritty.toml $HOME/.config/alacritty
+cp $HOME/LinuxConfigs/alacritty/burned-on-grey.toml $HOME/.config/alacritty
